a5fa5e0d2a3b3b0b0efe21eb3358f6c503866d04
KozlovOleg97 <116550710+KozlovOleg97@users.noreply.github.com>
2023-02-10 20:20:08 +0300

    Add files via upload

---
+++ CalculateEmployeePay.cs
@@ -0,0 +1,39 @@
+﻿using CalculateHoursForWorkLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateHoursForWorkLibrary
+{
+	public class CalculateEmployeePay
+	{
+		public static decimal GetLoop()
+		{
+			decimal loop = 0;
+
+			return loop;
+		}
+		public static decimal GetCalculateEmployeePay(List<TimeSheetEntryModel> timeSheets, EmployeeModel employee)
+		{
+			decimal output = 0;
+			double totalHours = 0;
+
+			for (int i = 0; i < timeSheets.Count; i++)
+			{
+				totalHours += timeSheets[i].HoursWorked;
+			}
+			if (totalHours > 40)
+			{
+				output = (((decimal)(totalHours - 40) * (employee.HourlyRate * 1.5M)) + (40M * employee.HourlyRate));
+			}
+			else
+			{
+				output = (decimal)totalHours * employee.HourlyRate;
+			}
+
+			return output;
+		}
+	}
+}
---
+++ DataAccess.cs
@@ -0,0 +1,14 @@
+﻿using CalculateHoursForWorkLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateHoursForWorkLibrary
+{
+	public static class DataAccess
+	{
+		
+	}
+}
