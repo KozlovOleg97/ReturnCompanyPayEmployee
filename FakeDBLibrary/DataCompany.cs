64448400e6aad985063a024312c71eadf326f67b
KozlovOleg97 <116550710+KozlovOleg97@users.noreply.github.com>
2023-02-11 13:25:09 +0300

    Add files via upload

---
+++ FakeDBLibrary/DataCompany.cs
@@ -0,0 +1,23 @@
+﻿using CalculateHoursForWorkLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakeDBLibrary
+{
+	public class DataCompany
+	{
+		public static List<CustomerModel> GetCompany()
+		{
+			List<CustomerModel> output = new List<CustomerModel>();
+
+			output.Add(new CustomerModel { CustomerName = "Acme", HourlyRateToBill = 150 });
+			output.Add(new CustomerModel { CustomerName = "ABC", HourlyRateToBill = 125 });
+
+			//jjjjjj
+			return output;
+		}
+	}
+}
---
+++ FakeDBLibrary/DataEmployee.cs
@@ -0,0 +1,24 @@
+﻿using CalculateHoursForWorkLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakeDBLibrary
+{
+	public class DataEmployee
+	{
+		public static EmployeeModel GetCurrentEmployee()
+		{
+			EmployeeModel output = new EmployeeModel
+			{
+				FirstName = "Jack",
+				LastName = "Peter",
+				HourlyRate = 15
+			};
+
+			return output;
+		}
+	}
+}
